ad3e51e1a95ea669644d35006d910180978aac74
ericcson24 <150656730+ericcson24@users.noreply.github.com>
2025-01-30 11:53:54 +0100

    Update typeDefs.ts

--- typeDefs.ts
+++ typeDefs.ts
@@ -1,30 +1,16 @@
 export const typeDefs = `#graphql
-    type Paciente {
-        id: ID!,
-        nombre: String!,
-        telefono: String!,
-        correo: String!
+    type X {
+       
         
     }
 
-    type Cita {
-        id: ID!,
-        Paciente: Paciente!,
-        fecha: String!,
-        tipo: String!
-        
-    }
     
     type Query {
         
-        getPacient(id:String!):Paciente!
-        getAppointments:[Cita!]!
+       
     }
 
     type Mutation {
-        addPatient(name:String!, telefono:String!,correo:String!):Paciente!
-        addAppointment(id:ID!,fecha:String!, tipo:String!):Cita!
-        deleteAppointment(id:ID!):boolean!
-        updatePacient(id:ID!, nombre:String, telefono:String,correo:String):Paciente!
+        
     }
 `
